f6f712cecddd19679124ee2fb5bbf5b9350df3db
SRAJAN GUPTA <srajan@ubuntu.ubuntu-domain>
2019-11-28 22:31:54 +0530

    connected to online database

--- util/database.js
+++ util/database.js
@@ -1,3 +1,3 @@
 const mysql = require('mysql2');
-const pool = mysql.createPool({ host: '127.0.0.1', user: 'srajan', password: 'Project@123', database: 'innovacer' });
+const pool = mysql.createPool({ host: 'sql12.freemysqlhosting.net', user: 'sql12313687', password: 'VuFaNpXUYF', database: 'sql12313687' });
 module.exports = pool.promise();
